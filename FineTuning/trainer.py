e3d699a539e4d59d9587134abb8cfd59d01557ca
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-08-25 09:46:39 +0545

    FineTuning Trainer

--- FineTuning/trainer.py
+++ FineTuning/trainer.py
@@ -1,7 +1,9 @@
 import torch
-import yaml 
-from transformers import AutoTokenizer, TrainingArguments, AutoModelForCausalLM, Trainer, BitsAndBytesConfig
+import yaml
+from datasets import load_from_disk
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from peft import LoraConfig, get_peft_model, TaskType
+from trl import SFTTrainer, SFTConfig
 
 
 
@@ -37,6 +39,44 @@ def load_lora_config(cfg):
         task_type= lora_cfg["task_type"]
     )
     return lora_config
+
+
+def get_training_config(cfg):
+    load_cfg = cfg["training"]
+    training_config = SFTConfig(
+        num_train_epochs= load_cfg["num_train_epochs"],
+        per_device_train_batch_size= load_cfg["per_device_train_batch_size"],
+        gradient_accumulation_steps= load_cfg["gradient_accumulation_steps"],
+        learning_rate= load_cfg["learning_rate"],
+        fp16= load_cfg["fp16"],
+        eval_steps= load_cfg["eval_steps"],
+        logging_steps= load_cfg["logging_steps"],
+        save_strategy= "epoch",
+        remove_unused_columns= load_cfg["remove_unused_columns"],
+        label_names= load_cfg["label_names"]
+    )
+    return training_config
+
+def train(cfg):
+    dataset= load_from_disk(cfg["data"]["path"])
+    model= load_model(cfg)
+    tokenizer= 0 # TODO: Add tokenizer
+    lora_config = load_lora_config(cfg)
+    model = get_peft_model(model, lora_config)
+    training_config = get_training_config(cfg)
+
+    trainer = SFTTrainer(
+        model= model,
+        args= training_config,
+        tokenizer= tokenizer,
+        train_dataset= dataset["train"],
+        eval_dataset= dataset["test"],
+    )
+
+    trainer.train()
+    model.save_pretrained(cfg["output"]["dir"])
+    tokenizer.save_pretrained(cfg["output"]["dir"])
+
 
 if __name__ == "__main__":
     cfg = load_config("./configs/qwen.yaml")
