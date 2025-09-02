45c30717da0dbb33078a4d9629e34444c190f670
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-09-02 19:43:51 +0545

    update

--- src/rag/query_translation/manager.py
+++ src/rag/query_translation/manager.py
@@ -1,12 +1,22 @@
 from .multi_query import multi_query
+from .decomposition import decompose_query
 
 class QueryTranslation:
     def __init__(self,cfg, vectorstore= None):
         self.vectorstore = vectorstore
-        self.chain = multi_query(cfg)
+        self.multiquery_chain = multi_query(cfg)
+        self.decompostion_chain = decompose_query(cfg)
+
 
 
     
-    def multiquery(self, question):
-        queries= self.chain.invoke({"questin": question})
-        return self._fetch_document(queries)    
+    def multiquery(self, question ):
+        queries= self.multiquery_chain.invoke({"questin": question})
+        return self._fetch_document(queries)
+
+    def decompose(self, question):
+        queries= self.decompostion_chain.invoke({"questin": question})
+    
+    def hyde():
+        pass
+    
