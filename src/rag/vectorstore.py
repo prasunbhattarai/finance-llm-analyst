6b7e8e06e12ae14e9acaf4c0d805f5c9baa6f0ef
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-09-04 19:50:56 +0545

    Updated

---
+++ src/rag/vectorstore.py
@@ -0,0 +1,21 @@
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+def vectorstore(docs, persist_dir="./chroma_db", model_name="sentence-transformers/all-MiniLM-L6-v2"):
+    embeddings = HuggingFaceEmbeddings(model_name= model_name)
+
+    vectordb = Chroma.from_documents(
+        embedding= embeddings,
+        persist_directory= persist_dir
+    )
+
+
+    collection_count = vectordb._collection.count()
+    if collection_count ==0:
+        vectordb.add_documents(docs)
+        vectordb.persist()
+        print(f"Added {len(docs)} documents to Chroma.")
+    else:
+        print(f"Vector store already has {collection_count} documents. Skipping addition.")
+    return vectordb.as_retriever(search_kwargs={"k":5})
