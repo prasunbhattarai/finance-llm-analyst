4d94d192969465e190a937948530c11e1298f95d
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-09-05 18:03:37 +0545

    Updated

--- src/rag/vectorstore.py
+++ src/rag/vectorstore.py
@@ -1,5 +1,8 @@
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import CrossEncoderReranker
+from sentence_transformers import CrossEncoder
 
 
 def vectorstore(docs, persist_dir="./chroma_db", model_name="sentence-transformers/all-MiniLM-L6-v2"):
@@ -18,4 +21,13 @@ def vectorstore(docs, persist_dir="./chroma_db", model_name="sentence-transforme
         print(f"Added {len(docs)} documents to Chroma.")
     else:
         print(f"Vector store already has {collection_count} documents. Skipping addition.")
-    return vectordb.as_retriever(search_kwargs={"k":5})
+
+    cross_encoder = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")   
+    reranker= CrossEncoderReranker(model=cross_encoder)
+
+    retriever = ContextualCompressionRetriever(
+        base_retriever= vectordb.as_retriever(search_kwargs={"k":10})
+        base_compressor= reranker
+    )
+    
+    return retriever
