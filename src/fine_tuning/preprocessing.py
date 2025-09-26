9fbf5c97fe6839e9d684caf6e540c95df899d8fc
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-09-26 20:24:32 +0545

    updated preprocessing

--- src/fine_tuning/preprocessing.py
+++ src/fine_tuning/preprocessing.py
@@ -1,18 +1,41 @@
 from datasets import load_dataset, concatenate_datasets
 from transformers import AutoTokenizer
 import yaml
+import os
 
-def load_config(path: str):
-    with open (path, 'r') as cfgs:
+def load_config(filename: str):
+    """
+    Load a YAML config file from the project's configs directory.
+    """
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    config_path = os.path.join(project_root, "configs", filename)
+
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Config file not found: {config_path}")
+
+    with open(config_path, "r") as cfgs:
         return yaml.safe_load(cfgs)
     
 cfg = load_config("../configs/qwen.yaml")
 
-dolly_dataset = load_dataset("databricks/databricks-dolly-15k", cache_dir="../datasets/raw/dolly")
-fiqa_dataset = load_dataset("llamafactory/fiqa", cache_dir="../datasets/raw/fiqa")
+dolly_dataset = load_dataset("databricks/databricks-dolly-15k", cache_dir= cfg["data"]["dolly_dir"])
+fiqa_dataset = load_dataset("llamafactory/fiqa", cache_dir= cfg["data"]["fiqa_dir"])
+finance_dataset = load_dataset("json", data_files= { "train": cfg["data"]["synthetic_dir"]})
 
 tokenizer = AutoTokenizer.from_pretrained(cfg["model"]["model_name"])
 tokenizer.pad_token = tokenizer.eos_token
+
+def finance_format(dataset):
+    rules = dataset["rules"]   
+    instruction = dataset["question"]
+    response = dataset["answer"]
+
+    prompt = f"### Rules:\n{rules}\n\n### Question:\n{instruction}"
+
+    return {
+        "text": f"{prompt}\n\n### Response:\n{response}"
+    }
+
 
 def dolly_format(dataset):
     instruction = dataset["instruction"]
@@ -20,12 +43,12 @@ def dolly_format(dataset):
     response = dataset["response"]
 
     if context:
-        prompt = f"Instruction:{instruction}\nContext:{context}\nAnswer:"
+        prompt = f"Instruction:{instruction}\nContext:{context}"
     else:
-        prompt = f"Instruction:{instruction}\nAnswer:"
+        prompt = f"Instruction:{instruction}:"
 
     return {
-        "text": f"### Instruction:\n{prompt}\n\n### Response:\n{response}"
+        "text": f"### \n{prompt}\n\n### Response:\n{response}"
     }
 
 
@@ -33,11 +56,11 @@ def fiqa_format(dataset):
     instruction = dataset["instruction"]
     inp= dataset["input"]
     output= dataset["output"]
-    prompt = f"Instruction:{instruction}\nInput:{inp}\nAnswer:"
+    prompt = f"Instruction:{instruction}\nInput:{inp}"
 
     
     return{
-        "text": f"### Instruction:\n{prompt}\n\n### Response:\n{output}"
+        "text": f"### {prompt}\n\n### Response:\n{output}"
     }
 
 
@@ -54,20 +77,24 @@ def tokenize(batch):
 
 dolly_dataset = dolly_dataset["train"].map(dolly_format, remove_columns=dolly_dataset["train"].column_names)
 fiqa_dataset = fiqa_dataset["train"].map(fiqa_format, remove_columns=fiqa_dataset["train"].column_names)
-
-tokenized_dolly = dolly_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1200))
-tokenized_fiqa = fiqa_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1200))
-
-test_dolly = dolly_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1100,2200))
-test_fiqa = fiqa_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1100,2200))
+finance_dataset = finance_dataset["train"].map(finance_format, remove_columns=finance_dataset["train"].column_names)
 
 
+tokenized_dolly = dolly_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1000))
+tokenized_fiqa = fiqa_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1000))
+tokenized_finance = finance_dataset.map(tokenize, batch_size=True, remove_columns=["text"]).select(range(1000))
 
-tokenized_dataset= concatenate_datasets([tokenized_dolly,tokenized_fiqa]).shuffle(seed=42)
-test_tokenized_dataset= concatenate_datasets([test_dolly,test_fiqa]).shuffle(seed=42)
+test_dolly = dolly_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1001,1100))
+test_fiqa = fiqa_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1001,1100))
+test_finance = finance_dataset.map(tokenize, batched=True, remove_columns=["text"]).select(range(1001,1100))
 
-test_tokenized_dataset.save_to_disk("../datasets/test/processed")
-tokenized_dataset.save_to_disk("../datasets/train/processed")
+
+
+tokenized_dataset= concatenate_datasets([tokenized_dolly,tokenized_fiqa, tokenized_finance]).shuffle(seed=42)
+test_tokenized_dataset= concatenate_datasets([test_dolly,test_fiqa, test_finance]).shuffle(seed=42)
+
+test_tokenized_dataset.save_to_disk(cfg["data"]["test_data"])
+tokenized_dataset.save_to_disk(cfg["data"]["train_data"])
 
 tokenizer.save_pretrained(cfg["output"]["dir"])
 
