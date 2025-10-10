249214b6137e9570edb5ed538e2c600bd94ce49a
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-10-10 20:40:37 +0545

    updated preprocessing

--- src/fine_tuning/preprocessing.py
+++ src/fine_tuning/preprocessing.py
@@ -1,18 +1,10 @@
 from datasets import load_dataset, concatenate_datasets
 from transformers import AutoTokenizer
-import yaml
-import os
+import yaml, os
 
 def load_config(filename: str):
-    """
-    Load a YAML config file from the project's configs directory.
-    """
     project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     config_path = os.path.join(project_root, "configs", filename)
-
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(f"Config file not found: {config_path}")
-
     with open(config_path, "r") as cfgs:
         return yaml.safe_load(cfgs)
     
@@ -22,7 +14,7 @@ def finance_format(dataset):
     rules = dataset["rules"]   
     instruction = dataset["question"]
     response = dataset["answer"]
-    prompt = f"### Rules:\n{rules}\n\n### Question:\n{instruction}"
+    prompt = f"### \n\n### Question:\n{instruction}"
     return {"text": f"{prompt}\n\n### Response:\n{response}"}
 
 def dolly_format(dataset):
@@ -42,7 +34,6 @@ def fiqa_format(dataset):
     prompt = f"Instruction: {instruction}\nInput: {inp}"
     return {"text": f"### {prompt}\n\n### Response:\n{output}"}
 
-# ---------------- Tokenization ---------------- #
 def tokenize(batch, tokenizer):
     token = tokenizer(
         batch["text"],
@@ -53,18 +44,21 @@ def tokenize(batch, tokenizer):
     token["labels"] = token["input_ids"].copy()
     return token
 
-# ---------------- Main dataset pipeline ---------------- #
-def prepare_datasets(cfg: dict, test_size: float = 0.1, seed: int = 42):
-    dolly_dataset = load_dataset("databricks/databricks-dolly-15k", cache_dir=cfg["data"]["dolly_dir"])
-    fiqa_dataset = load_dataset("llamafactory/fiqa", cache_dir=cfg["data"]["fiqa_dir"])
-    finance_dataset = load_dataset("json", data_files={"train": cfg["data"]["synthetic_dir"]})
+def prepare_datasets(cfg: dict, test_size: float = 0.1, seed: int = 42, target_size = 1700):
+    dolly_dataset = load_dataset("databricks/databricks-dolly-15k", cache_dir=cfg["data"]["dolly_dir"])["train"]
+    fiqa_dataset = load_dataset("llamafactory/fiqa", cache_dir=cfg["data"]["fiqa_dir"])["train"]
+    finance_dataset = load_dataset("json", data_files={"train": cfg["data"]["synthetic_dir"]})["train"]
 
     tokenizer = AutoTokenizer.from_pretrained(cfg["model"]["model_name"])
     tokenizer.pad_token = tokenizer.eos_token
 
-    dolly_dataset = dolly_dataset["train"].map(dolly_format, remove_columns=dolly_dataset["train"].column_names)
-    fiqa_dataset = fiqa_dataset["train"].map(fiqa_format, remove_columns=fiqa_dataset["train"].column_names)
-    finance_dataset = finance_dataset["train"].map(finance_format, remove_columns=finance_dataset["train"].column_names)
+    dolly_dataset = dolly_dataset.shuffle(seed=seed).select(range(target_size))
+    fiqa_dataset = fiqa_dataset.shuffle(seed=seed).select(range(target_size))
+    finance_dataset = finance_dataset.shuffle(seed=seed).select(range(target_size))
+
+    dolly_dataset = dolly_dataset.map(dolly_format, remove_columns=dolly_dataset.column_names)
+    fiqa_dataset = fiqa_dataset.map(fiqa_format, remove_columns=fiqa_dataset.column_names)
+    finance_dataset = finance_dataset.map(finance_format, remove_columns=finance_dataset.column_names)
 
     dolly_split = dolly_dataset.train_test_split(test_size=test_size, seed=seed)
     fiqa_split = fiqa_dataset.train_test_split(test_size=test_size, seed=seed)
@@ -86,6 +80,7 @@ def prepare_datasets(cfg: dict, test_size: float = 0.1, seed: int = 42):
     test_dataset.save_to_disk(cfg["data"]["test_data"])
 
     tokenizer.save_pretrained(cfg["output"]["dir"])
+
 
     return train_dataset, test_dataset, tokenizer
 
